20d3e34ac27aad8c428a0624479e850e618f6177
Benoit Saccomano <bainwa@gmail.com>
2011-05-12 15:29:44 +0200

    Advanced comportement

--- quadriMotion.py
+++ quadriMotion.py
@@ -3,23 +3,33 @@ from math import sqrt
 
 
 def main():
+    
     # CONSTANTE
-    GRAVITY = 9.81
-    MOTOR_MAX = 10
+    GRAVITY = 9.800
+    MOTOR_MAX = 5
     RAYON = 1
     RATIO = sqrt(2)/2
     LACET = 0.5
+    F = (1.3 * GRAVITY)/4
+    FDOWN = (0.2 * GRAVITY)/4
     
-    MODE = "Basic"
+    MODE = "Ba"
     
     # INIT
     cont = bge.logic.getCurrentController()
     own = cont.owner
+    
+    ForceFrontRight = 0
+    ForceFrontLeft = 0
+    ForceRearRight = 0
+    ForceRearLeft = 0
+    
 
     keyUp = cont.sensors['Up']
     keyDown = cont.sensors['Down']
     keyLeft = cont.sensors['Left']
     keyRight = cont.sensors['Right']
+    
     keyZ = cont.sensors['Z']
     keyS = cont.sensors['S']
     keyQ = cont.sensors['Q']
@@ -36,18 +46,45 @@ def main():
     MotRearLeft = cont.actuators['MotRearLeft']
     
     # BEHAVIOR MODEL
+    if keyZ.positive:
+        ForceFrontRight += F
+        ForceFrontLeft += F
+        ForceRearRight += F
+        ForceRearLeft += F
+        
+    if keyS.positive:
+        ForceFrontRight += -FDOWN
+        ForceFrontLeft += -FDOWN
+        ForceRearRight += -FDOWN
+        ForceRearLeft += -FDOWN
+        
+    if keyD.positive: #TODO
+        ForceFrontRight = F 
+        ForceFrontLeft = ForceFrontLeft - 1
+        ForceRearRight = ForceRearRight - 1
+        ForceRearLeft = ForceRearLeft - 1
+        
+    if keyQ.positive: #TODO
+        ForceFrontRight = ForceFrontRight - 1 
+        ForceFrontLeft = ForceFrontLeft - 1
+        ForceRearRight = ForceRearRight - 1
+        ForceRearLeft = ForceRearLeft - 1    
+            
     if keyUp.positive:
-        MotFrontRight.force = [0, 0, 12]
+        ForceRearRight += F
+        ForceRearLeft += F
 
     if keyDown.positive:
-        MotFrontLeft.force = [0, 0, 12]
+        ForceFrontRight += F 
+        ForceFrontLeft += F
 
     if keyLeft.positive:
-        MotRearRight.force = [0, 0, 12]
+        ForceFrontRight += F 
+        ForceRearRight += F
 
     if keyRight.positive:
-        MotRearLeft.force = [0, 0, 12]
-    
+        ForceFrontLeft += F
+        ForceRearLeft += F
     
     # NO KEY MOTION
     MotFrontLeft.torque = [0, 0, 0]
@@ -81,7 +118,17 @@ def main():
         if key2.positive:
             MotRearRight.force = [0, 0, MOTOR_MAX]
             MotRearRight.torque = [-1 * MOTOR_MAX * RATIO, -1 * MOTOR_MAX * RATIO, -LACET]
+    else:
+        MotFrontLeft.force = [0, 0, ForceFrontLeft] 
+        MotFrontLeft.torque = [ForceFrontLeft * RATIO, ForceFrontLeft * RATIO, -LACET]
         
+        MotFrontRight.force = [0, 0, ForceFrontRight]
+        MotFrontRight.torque = [ForceFrontRight * RATIO, -1 * ForceFrontRight * RATIO, LACET] 
         
+        MotRearRight.force = [0, 0, ForceRearRight]
+        MotRearRight.torque = [-1 * ForceRearRight * RATIO, -1 * ForceRearRight * RATIO, -LACET]
+         
+        MotRearLeft.force = [0, 0, ForceRearLeft]
+        MotRearLeft.torque = [-1 * ForceRearLeft * RATIO, ForceRearLeft * RATIO, LACET] 
 
 main()
